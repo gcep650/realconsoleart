ecad514a7b6e74a2f3c691783561242ef9ad6177
gcep650 <gcep650@learner.dvusd.org>
2019-08-15 09:16:28 -0700

    Add project files.

---
+++ ConsolePics/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace ConsolePics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("  !");
+            WriteLine("(O.O)");
+            WriteLine("  |  ");
+            WriteLine("_/|\\_");
+            WriteLine("  |  ");
+            WriteLine("_/ \\_");
+            WriteLine("\n");
+            ReadLine();
+
+            WriteLine("||||||||\t|||||||||||\t _______");
+            WriteLine("||||||||\t |||||||||\t/\t\\");
+            WriteLine("||||||||\t  |||||||      /\t \\");
+            WriteLine("||||||||\t   |||||      /\t\t  \\");
+            WriteLine("||||||||\t    |||       \\\t	  /");
+            WriteLine("||||||||\t     |	       \\\t /");
+            WriteLine("||||||||\t                \\_______/");
+            WriteLine("\n");
+            ReadLine();
+
+            WriteLine(" GGGGGGG\t CCCCCC");
+            WriteLine("G\tG\tC      C");
+            WriteLine("G\t\tC");
+            WriteLine("G    GGGG\tC");
+            WriteLine("G       G\tC      C");
+            WriteLine(" GGGGGGG\t CCCCCC");
+            ReadLine();
+        }
+    }
+}
